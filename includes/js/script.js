3a281d7e17b9da13f9ba88d5831ec0461ac31253
12btcse1108 <nitinverma1394@gmail.com>
2016-10-18 13:59:39 +0530

    My Bootstrap3.0 file

---
+++ includes/js/script.js
@@ -0,0 +1,14 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$('a.pop').click(function(e){
+  e.preventDefault();
+});
+$('a.pop').popover();
